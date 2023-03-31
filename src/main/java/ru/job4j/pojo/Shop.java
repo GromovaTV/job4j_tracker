4e9a68353bdfd3a10282f4fce202aca81248cdbc
Tatyana <quininthenorth@gmail.com>
2023-03-31 12:46:55 +0400

    Массив с пустыми ячейками

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,26 @@
+package ru.job4j.pojo;
+
+public class Shop {
+
+    public static int indexOfNull(Product[] products) {
+        int index = -1;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            /* проверяем, что объект не равен null. Т.к. у нас массив не заполнен целиком. */
+            if (product == null) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+        System.out.println(indexOfNull(products));
+    }
+}

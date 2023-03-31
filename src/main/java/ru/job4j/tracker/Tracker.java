dd66416c2dc4eef9a052dd66b30b7d663b99d865
Tatyana <quininthenorth@gmail.com>
2023-03-31 16:32:05 +0400

    Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -20,6 +23,34 @@ public class Tracker {
                 break;
             }
         }
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        int i = 0;
+        Item[] rsl = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] == null) {
+                continue;
+            }
+            if (items[index].getName().equals(key)) {
+                rsl[i++] = items[index];
+            }
+        }
+        rsl = Arrays.copyOf(rsl, i);
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        int i = 0;
+        Item[] rsl = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] == null) {
+                continue;
+            }
+            rsl[i++] = items[index];
+        }
+        rsl = Arrays.copyOf(rsl, i);
         return rsl;
     }
 }

8a22990f0767f1c327f2719dddce416ae5b02537
Tatyana <quininthenorth@gmail.com>
2023-04-05 15:59:22 +0400

    Пользовательские исключения

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -10,7 +10,7 @@ public class FindEl {
             }
         }
         if (rsl == -1) {
-            throw new ElementNotFoundException("Element is not foud");
+            throw new ElementNotFoundException("Element is not found");
         }
         return rsl;
     }

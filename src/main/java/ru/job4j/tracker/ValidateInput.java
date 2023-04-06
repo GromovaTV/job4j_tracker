87a4d303211a9605a8232699129220cb8cca54cc
Tatyana <quininthenorth@gmail.com>
2023-04-06 12:29:35 +0400

    Тесты на StartUI

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -11,7 +11,7 @@ public class ValidateInput extends ConsoleInput {
                 value = super.askInt(question);
                 invalid = false;
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter valida data again.");
+                System.out.println("Please enter valid data again.");
             }
         } while (invalid);
         return value;

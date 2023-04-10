7d8c6f7dc1ee7f6f9782b05ca919e9455332258d
Tatyana <quininthenorth@gmail.com>
2023-04-10 13:29:28 +0400

    Map, HashMap

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("ivanov@yandex.ru", "Ivanov Ivan Ivanovich");
+        map.put("petrov@yandex.ru", "Petrov Ivan Ivanovich");
+        map.put("smirnov@yandex.ru", "Smirnov Ivan Ivanovich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}

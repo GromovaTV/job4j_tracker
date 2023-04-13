3524ab829f4aa3d023ae1ba415e51ea26304667b
Tatyana <quininthenorth@gmail.com>
2023-04-13 18:06:43 +0400

    Встроенные функциональные интерфейсы

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -5,18 +5,6 @@ import java.util.function.Predicate;
 
 public class SearchFolder {
 
-    public boolean size(Folder f) {
-        return check(str -> str.getSize() > 100, f);
-    }
-
-    public boolean contains(Folder f) {
-        return check(str -> str.getName().contains("bug"), f);
-    }
-
-    public boolean check(Predicate<Folder> pred, Folder f) {
-        return pred.test(f);
-    }
-
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
         List<Folder> rsl = new ArrayList<>();
         for (Folder f : list) {

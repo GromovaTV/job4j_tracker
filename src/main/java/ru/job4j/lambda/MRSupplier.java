5224ef63fc37461eda1d2febbc833c7dd1fffaa7
Tatyana <quininthenorth@gmail.com>
2023-04-17 13:10:59 +0400

    Method reference и Supplier

---
+++ src/main/java/ru/job4j/lambda/MRSupplier.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.function.Supplier;
+
+/**
+ * Интерфейс Supplier служит для того, чтобы обернуть логику создания объекта.
+ * Он ничего не принимает, только возвращает объект, который ему поручили создать.
+ * Ссылки на методы поддерживает метод new, который позволяет нам создать нужный нам объект
+ *
+ * Вернуть Supplier, который создает пустую строку через new.
+ */
+public class MRSupplier {
+    public static Supplier<String> supplier() {
+        return String::new;
+    }
+}

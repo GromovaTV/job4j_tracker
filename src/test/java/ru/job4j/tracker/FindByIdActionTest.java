1d7a2c29e37966d5133b277d4726a65526dc5d54
Tatyana <quininthenorth@gmail.com>
2023-07-17 15:35:16 +0400

    Mock-объекты

---
+++ src/test/java/ru/job4j/tracker/FindByIdActionTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class FindByIdActionTest {
+
+    @Test
+    public void execute() {
+        Output out = new StubOutput();
+        Store tracker = new MemTracker();
+        Item item = tracker.add(new Item("Item"));
+        FindByIdAction res = new FindByIdAction(out);
+        Input input = mock(Input.class);
+        when(input.askInt(any(String.class))).thenReturn(1);
+        res.execute(input, tracker);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is("=== Find item by id ===" + ln + item.toString() + ln));
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/FindByNameActionTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class FindByNameActionTest {
+
+    @Test
+    public void execute() {
+        Output out = new StubOutput();
+        Store tracker = new MemTracker();
+        Item item = tracker.add(new Item("Item"));
+        FindByNameAction res = new FindByNameAction(out);
+        Input input = mock(Input.class);
+        when(input.askStr(any(String.class))).thenReturn("Item");
+        res.execute(input, tracker);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is("=== Find items by name ===" + ln + item.toString() + ln));
+    }
+}
